84835778a23c94458fe68e9765b04ebaac399b99
BIT <BIT@DESKTOP-9RGLHVD>
2022-07-13 19:57:31 +0900

    first commit

---
+++ src/selfstudy/DefaultValue.java
@@ -0,0 +1,12 @@
+package selfstudy;
+
+public class DefaultValue {
+	public static void main(String[] args) {
+		Solution a = new Solution();
+		int[] b = {5, 4, 1, 2, 3};
+		
+		System.out.println("answer is "+a.solution(b));
+		//System.out.println(a.solution({1, 2, 3}));
+		//¿Ã∞« ø÷ æ»µ≈?
+	}
+}
---
+++ src/selfstudy/Solution.java
@@ -0,0 +1,31 @@
+package selfstudy;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int[] nums) {
+    	int[] input = nums; //배열 받아와 input 변수에 저장
+//    	System.out.println(Arrays.toString(input));
+    	Arrays.sort(input); // 배열 정렬
+//    	System.out.println(Arrays.toString(input));
+    	
+    	//소수리스트 만들기(최대값까지)
+    	//깃 보내고싶은데
+    	int leng = input.length-1;
+//    	System.out.println(leng);
+    	int maxi = input[leng]+input[leng-1]+input[leng-2];
+//    	System.out.println(maxi);
+    	int[] sosu = {2, 3, 5, 7};
+    	if (sosu[sosu.length-1]<maxi) {
+    		for (int i = 1; sosu[sosu.length-1]>=maxi; i++) {
+			}
+    	}
+    	
+    	
+        int answer = -1;
+        
+        return answer;
+    }
+
+}
