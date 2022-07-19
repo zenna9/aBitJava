094daa366ca18d7eb0a84196056dad69d78cd139
BIT <BIT@DESKTOP-9RGLHVD>
2022-07-19 18:30:43 +0900

    진짜진짜 마지막

---
+++ src/selfstudy/B0719_SplitInput.java
@@ -0,0 +1,12 @@
+package selfstudy;
+
+import java.util.Scanner;
+
+public class B0719_SplitInput {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String inputs = input.next();
+		
+		System.out.println(inputs);
+	}
+}
