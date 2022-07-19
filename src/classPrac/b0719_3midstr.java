fa3338d952fff90cc86226092dfd277b0e194c34
BIT <BIT@DESKTOP-9RGLHVD>
2022-07-19 17:38:35 +0900

    22.07.19

---
+++ src/classPrac/b0719_3midstr.java
@@ -0,0 +1,37 @@
+package classPrac;
+
+import java.util.Scanner;
+
+public class b0719_3midstr {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("put an Stri오오오o split :");
+		String s = input.next();
+		System.out.print("Index to start :");
+		String a = input.next();
+		System.out.print("how many : ");
+		String b = input.next();
+		
+		String result = midstr(s, a, b);
+		System.out.println("result is : "+result);
+	}
+	private static String midstr(String...str) {
+		String answ = "";
+		String s = str[0];
+		int a = Integer.parseInt(str[1]);
+		int b = Integer.parseInt(str[2]);
+		if(a<0||a>s.length()-1) {
+			System.out.println("index to start is wrong");
+			System.exit(0);
+		}else if(b<=0||(s.length()-1)<b+a) {
+			System.out.println("your last input is wrong");
+			System.exit(0);
+		}else {
+			answ = s.substring(a,a+b);
+		}
+		 
+		return answ;
+	}
+
+}
--- src/programmers/Lv1_PrimeNumber.java
+++ src/programmers/Lv1_PrimeNumber.java
@@ -1,33 +1,13 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Lv1_PrimeNumber {
 
 	public static void main(String[] args) {
-		int[] b = { 5, 4, 1, 2, 3 };
-		int ans = solution(b);
-		System.out.println("answer is " + ans);
-		// System.out.println(a.solution({1, 2, 3}));
-		// 이건 왜 안돼?
-	}
-
-	public static int solution(int[] nums) {
-		
-		int answer = 0;
-		ArrayList<Integer> sumlist = new ArrayList<Integer>();
-		for (int i = 0; i < nums.length-2; i++) {
-			for (int j = i+1; j < nums.length-1; j++) {
-				for (int j2 = j+1; j2 < nums.length; j2++) {
-					sumlist.add(nums[i]+nums[j]+nums[j2]);
-				}
-			}
-		}
-		for (int i : sumlist) {
-			System.out.print(i+",");
-		}
-		System.out.println();
-		return answer;
+		String a = "apple";
+		String b = "apple";
+		boolean trues = (a==b);
+		boolean trues2 = a.equals(b);
+		System.out.println(trues);
+		System.out.println(trues2);
 	}
 }
